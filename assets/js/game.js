c820d9713a11a2dfad810b0702b8f22ef881ae05
Yosemite <johnsen.luke@gmail.com>
2021-09-19 20:04:37 -0700

    learning functions and variables in js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-window.alert ("This is an alert! JavaScripts is running!")
+var playerName = window.prompt("What is your robot's name?");
+console.log (playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+// fight();
