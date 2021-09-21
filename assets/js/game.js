4a7d7146a496f4ce04a8e80ea29efbcca840582a
Yosemite <johnsen.luke@gmail.com>
2021-09-20 21:33:43 -0700

    multiple rounds and alerts complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -50,21 +50,28 @@ var fight = function (enemyName) {
         );
 
 
-        if (playerHealth <= 0) {
-            window.alert(playerName + " has died!");
-            break;
-        } else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
-        }
+            if (playerHealth <= 0) {
+                window.alert(playerName + " has died!");
+                break;
+            } else {
+                window.alert(playerName + " still has " + playerHealth + " health left.");
+            }
 
-    } else {
-        window.alert("You need to pick a valid option. Try again!");
-    }
+        } else {
+            window.alert("You need to pick a valid option. Try again!");
+        }
     }
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
 }
